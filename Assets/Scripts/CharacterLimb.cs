1014e8716c1d86122aa166e3efaab3afaca9596b
PeterSchlenker <91510378+PeterSchlenker@users.noreply.github.com>
2024-04-27 14:03:04 -0500

    Added characters 2 - 4

---
+++ Assets/Scripts/CharacterLimb.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class CharacterLimb : MonoBehaviour {
+    public Sprite[] sprites;
+
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        spriteRenderer.sprite = sprites[GameManager.instance.personIndex];
+    }
+}
